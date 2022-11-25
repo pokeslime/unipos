97746497044f77c79ef40d719b3b11ee60ab298d
pokeslime <111343037+pokeslime@users.noreply.github.com>
2022-11-25 13:17:23 +0900

    unipos resolved compile error

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, BrowserRouter } from 'react-router-dom';
+import { LoginOrRegister } from './components/LoginOrRegister';
+import { LoginPage } from './components/LoginPage';
+import { RegisterPage } from './components/RegisterPage';
+import { HomePage } from './components/HomePage';
+import { MemberPage } from './components/MemberPage';
+import { ContributionPage } from './components/ContributionPage'; 
+import { DeletePage } from './components/DeletePage';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Route exact path="/" component={LoginOrRegister} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={RegisterPage} />
+        <Route path="/home" component={HomePage} />
+        <Route path="/member" component={MemberPage} />
+        <Route path="/contribution" component={ContributionPage} />
+        <Route path="delete" component={DeletePage} />
+  </BrowserRouter>
   );
-}
-
-export default App;
+};
